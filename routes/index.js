c2c526ab3309b9089f5d1df36374d288688eafbb
DeclanElk <delkerton2001@gmail.com>
2021-09-05 22:29:06 +1000

    Migrating to pug from jade

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.writeHead(200, {'content-type': 'text/html'});
-  res.write(createHtml());
-  res.write(`<br><br><br>`);
-  res.end();
+  res.render('index')
 });
 
 module.exports = router;
